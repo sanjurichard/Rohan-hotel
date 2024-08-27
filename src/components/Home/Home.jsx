2d61df134478751919460db5c04fef5708b778f0
sanjuRichard <sanjurichard0@gmail.com>
2024-08-27 12:39:44 +0530

    changesnew

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
 import './Home.css';
 import logo from '../../assets/rohanlogo.png';
+import image1 from '../../assets/1.png';
+import image2 from '../../assets/2.png';
+import image3 from '../../assets/3.png';
+import image4 from '../../assets/4.png';
+import image5 from '../../assets/5-1.png';
 import resrev from '../../assets/rohanreserv.png';
+import cleartrip from '../../assets/cleartrip.png';
+import agoda from '../../assets/agoda.png';
+import booking from '../../assets/booking.png';
+import expedia from '../../assets/expedia.jpg';
+import goibo from '../../assets/goibo.png';
+import hotels from '../../assets/hotels.png';
+import mmtrip from '../../assets/mmtrip.png';
+import transadvisor from '../../assets/tranadvisor.png';
+import trip from '../../assets/trip.png';
+import vio from '../../assets/vio.png';
+import htg from '../../assets/htg.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
 import { faCalendar as faCalendarRegular } from '@fortawesome/free-regular-svg-icons';
@@ -30,6 +46,20 @@ const Home = () => {
             setKids(kids - 1);
         }
     };
+
+    const partners = [
+        { name: 'Cleartrip.com', price: '₹2,160', cancellation: 'Free cancellation until 11 Jun', url: 'https://www.cleartrip.com', imgSrc: cleartrip },
+        { name: 'Goibibo.com', price: '₹2,398', cancellation: 'Free cancellation until 11 Jun',imgSrc: goibo },
+        { name: 'Agoda', price: '₹2,419', cancellation: 'Free cancellation until 10 Jun',imgSrc: agoda  },
+        { name: 'MakeMyTrip.com', price: '₹2,489', cancellation: 'Free cancellation until 11 Jun',imgSrc: mmtrip  },
+        { name: 'Tripadvisor.in', price: '₹2,419', cancellation: 'Free cancellation until 10 Jun' ,imgSrc: transadvisor },
+        { name: 'Vio.com', price: '₹2,419', cancellation: 'Free cancellation until 11 Jun',imgSrc: vio  },
+        { name: 'Booking.com', price: '₹3,427', cancellation: 'Free cancellation until 9 Jun',imgSrc: booking  },
+        { name: 'Hotels.com', price: '₹2,846', cancellation: 'Free cancellation until 11 Jun' ,imgSrc: hotels },
+        { name: 'Expedia.co.in', price: '₹2,846', cancellation: 'Free cancellation until 11 Jun' ,imgSrc: expedia },
+        { name: 'Trip.com', price: '₹3,698', cancellation: 'Free cancellation until 11 Jun',imgSrc: trip },
+        { name: 'HomeToGo.com', price: '₹2,568', cancellation: 'Free cancellation until 11 Jun',imgSrc: htg },
+    ];
 
     return (
         <div>
@@ -124,6 +154,50 @@ const Home = () => {
                     <img src={resrev} alt="Rohaan Hotels Logo" />
                 </div>
             </section>
+            <div className="home-container">
+                <h2 className='title'>Our Rooms</h2>
+                <div className="image-grid-wrapper">
+                    <div className="large-image-container">
+                        <img src={image5} alt="Image 5" className="large-image" />
+                    </div>
+                    <div className="small-images-container">
+                        <div className="small-image-row">
+                            <img src={image1} alt="Image 1" className="small-image" />
+                            <img src={image2} alt="Image 2" className="small-image" />
+                        </div>
+                        <div className="small-image-row">
+                            <img src={image3} alt="Image 3" className="small-image" />
+                            <img src={image4} alt="Image 4" className="small-image" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="comparison-container">
+                <div className="comparison-text">
+                    <h1>Compare and book from our trusted partners.</h1>
+                    <p>Easily compare and book services from our trusted partners, ensuring you get the best options available with confidence.</p>
+                </div>
+                <div className="comparison-list">
+                    {partners.map((partner, index) => (
+                        <div key={index} className="partner-item">
+                            <div className="partner-info">
+
+                                <img className="partner-logo" src={partner.imgSrc} alt={`${partner.name} logo`} />
+                                <div className="partner-details">
+                                    <h2>{partner.name}</h2>
+                                    <p>{partner.cancellation}</p>
+                                </div>
+                            </div>
+                            <div className="partner-price">
+                                <p>{partner.price}</p>
+                                <button onClick={() => window.open(partner.url, '_blank')}>Visit site</button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
         </div>
     );
 };
